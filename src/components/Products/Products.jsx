9a1978559a5224d95bcabe64345b8ddd17c5e23c
sibTunechy <ayotemitope930@gmail.com>
2022-03-24 19:11:36 +0100

    e-commerce begins

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Grid from '@material-ui/core';
+
+const products = [
+    { id: 1, name: 'Shoes', description: 'Running Shoes.' },
+    { id: 2, name: 'Macbook', description: 'Apple Laptop' },    
+];
+
+const Products = () => {
+    <main>
+        <Grid container justify='center' spacing={4}>
+
+        </Grid>
+    </main>
+}
+
+export default Products;
