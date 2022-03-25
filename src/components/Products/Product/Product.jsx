ec6a917a5d037d04dbb97a4be90a2fd73b78ab5f
sibTunechy <ayotemitope930@gmail.com>
2022-03-25 14:02:13 +0100

    more changes

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton  } from '@material-ui/core';
+import { AddShoppingCart, CallMissedSharp } from '@material-ui/icons';
+
+const Product = () => {
+  return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant='h5'>
+                        {}
+                    </Typography>
+                </div>
+            </CardContent>
+        </Card>
+  )
+}
+
+export default Product
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import Grid from '@material-ui/core';
 
 const products = [
-    { id: 1, name: 'Shoes', description: 'Running Shoes.' },
-    { id: 2, name: 'Macbook', description: 'Apple Laptop' },    
+    { id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$5' },
+    { id: 2, name: 'Macbook', description: 'Apple Laptop', price: '$15' },    
 ];
 
 const Products = () => {
     <main>
         <Grid container justify='center' spacing={4}>
-
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product}/>
+                <Grid/>
+            ))}
         </Grid>
     </main>
 }
