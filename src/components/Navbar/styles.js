10f240b97b4a8f479d01e2ec4b9d3de078affe5c
sibTunechy <ayotemitope930@gmail.com>
2022-03-31 10:41:57 +0100

    more styles

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -4,6 +4,9 @@ const  drawerWidth = 0;
 
 export default makeStyles((theme) => ({
     appBar: {
+        boxshadow: 'none',
+        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+
 
     }
 })) // more styles here to design the already structured Navbar.....
