551e613368417660872a3a23cfb02fecec1873a6
sibTunechy <ayotemitope930@gmail.com>
2022-04-01 19:31:02 +0100

    more styles added

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -6,7 +6,35 @@ export default makeStyles((theme) => ({
     appBar: {
         boxshadow: 'none',
         borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-
-
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+        },
+    },
+    title: {
+        flexGrow: 1,
+        alignItems: 'center',
+        display: 'flex',
+        textDecoration: 'none',
+    },
+    image: {
+        marginRight: '10px',
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
     }
 })) // more styles here to design the already structured Navbar.....
