1ae422c3926cee2f1d8bb39057c224e612fd12a5
sibTunechy <ayotemitope930@gmail.com>
2022-04-02 22:44:16 +0100

    more products added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 
 import { Products, Navbar } from './components';
 
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);  // this useEffect has a dependency array set to empty which means its only going to run at the start of the render.
+
+  console.log(products);
+
   return (
     <div>
         <Navbar/>
