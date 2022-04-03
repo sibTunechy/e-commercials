5902832ee5a1b4024523ce5be54317ad6836f756
sibTunechy <ayotemitope930@gmail.com>
2022-04-03 23:40:08 +0100

    more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import { commerce } from './lib/commerce';
+import { commerce } from './lib/commerce';
 
 import { Products, Navbar } from './components';
 
